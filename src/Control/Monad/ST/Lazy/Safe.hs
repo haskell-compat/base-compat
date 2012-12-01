d45decba547c0e52df247a4ae63620882ba595d2
Simon Hengel <sol@typeful.net>
2012-12-01 19:41:55 +0100

    GHC 7.0.4 compatibility

--- src/Control/Monad/ST/Lazy/Safe.hs
+++ src/Control/Monad/ST/Lazy/Safe.hs
@@ -1,3 +1,9 @@
 {-# LANGUAGE PackageImports #-}
-module Control.Monad.ST.Lazy.Safe (module Base) where
+module Control.Monad.ST.Lazy.Safe (
+#if MIN_VERSION_base(4,4,0)
+  module Base
+) where
 import "base" Control.Monad.ST.Lazy.Safe as Base
+#else
+) where
+#endif
--- src/Control/Monad/ST/Lazy/Unsafe.hs
+++ src/Control/Monad/ST/Lazy/Unsafe.hs
@@ -1,3 +1,9 @@
 {-# LANGUAGE PackageImports #-}
-module Control.Monad.ST.Lazy.Unsafe (module Base) where
+module Control.Monad.ST.Lazy.Unsafe (
+#if MIN_VERSION_base(4,4,0)
+  module Base
+) where
 import "base" Control.Monad.ST.Lazy.Unsafe as Base
+#else
+) where
+#endif

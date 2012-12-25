86428dc97b4389f0afab118cf545a0b516d082d7
Simon Hengel <sol@typeful.net>
2012-12-25 16:03:58 +0100

    Add isLeft/isRight

--- src/Data/Either.hs
+++ src/Data/Either.hs
@@ -1,4 +1,18 @@
 module Data.Either (
   module Base
+, isLeft
+, isRight
 ) where
 import "base" Data.Either as Base
+
+import Prelude
+
+-- | Return `True` if the given value is a `Left`-value, `False` otherwise.
+isLeft :: Either a b -> Bool
+isLeft (Left  _) = True
+isLeft (Right _) = False
+
+-- | Return `True` if the given value is a `Right`-value, `False` otherwise.
+isRight :: Either a b -> Bool
+isRight (Left  _) = False
+isRight (Right _) = True

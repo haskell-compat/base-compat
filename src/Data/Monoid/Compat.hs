f34c4cf6cd89cbb1c03fb1985ade447a4c160dd7
Simon Hengel <sol@typeful.net>
2014-03-08 08:17:35 +0100

    Add explicit export list to Data.Monoid.Compat

--- src/Data/Monoid/Compat.hs
+++ src/Data/Monoid/Compat.hs
@@ -1,11 +1,29 @@
 module Data.Monoid.Compat (
-  module Base
-, (<>)
-) where
+        -- * Monoid typeclass
+        Monoid(..),
+        (<>),
+        Dual(..),
+        Endo(..),
+        -- * Bool wrappers
+        All(..),
+        Any(..),
+        -- * Num wrappers
+        Sum(..),
+        Product(..),
+        -- * Maybe wrappers
+        -- $MaybeExamples
+        First(..),
+        Last(..)
+  ) where
+
 import Data.Monoid as Base
 
 #if !MIN_VERSION_base(4,5,0)
+infixr 6 <>
+
 -- | An infix synonym for 'mappend'.
+--
+-- /Since: 4.5.0.0/
 (<>) :: Monoid m => m -> m -> m
 (<>) = mappend
 {-# INLINE (<>) #-}

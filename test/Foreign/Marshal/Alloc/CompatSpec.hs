e8e78a76d9b8bc0cd8be61f5ff8d5a27e6e6351e
Simon Hengel <sol@typeful.net>
2015-04-06 15:43:55 +0800

    Simplify test for Foreign.Alloc.Compat.calloc

--- test/Foreign/Marshal/Alloc/CompatSpec.hs
+++ test/Foreign/Marshal/Alloc/CompatSpec.hs
@@ -2,6 +2,7 @@ module Foreign.Marshal.Alloc.CompatSpec (main, spec) where
 
 import           Test.Hspec
 
+import           Control.Exception.Compat
 import           Foreign.Marshal.Alloc.Compat
 import           Foreign.Marshal.Utils
 import           Foreign.Ptr
@@ -14,10 +15,5 @@ spec :: Spec
 spec = do
   describe "calloc" $
     it "allocates memory with bytes of value zero" $ do
-        intPtr1 <- calloc :: IO (Ptr Int)
-        intPtr2 <- new 0
-        val1 <- peek intPtr1
-        val2 <- peek intPtr2
-        val1 `shouldBe` val2
-        free intPtr1
-        free intPtr2
+      bracket calloc free $ \ptr -> do
+        peek ptr `shouldReturn` (0 :: Int)

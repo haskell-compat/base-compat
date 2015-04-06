df86a3f5ed502417b79b3c47b95760d3a734089b
RyanGlScott <ryan.gl.scott@ku.edu>
2015-04-06 01:20:31 -0500

    Trailing newlines, no need for QuickCheck test

--- test/Foreign/Marshal/Alloc/CompatSpec.hs
+++ test/Foreign/Marshal/Alloc/CompatSpec.hs
@@ -1,7 +1,6 @@
 module Foreign.Marshal.Alloc.CompatSpec (main, spec) where
 
 import           Test.Hspec
-import           Test.QuickCheck
 
 import           Foreign.Marshal.Alloc.Compat
 import           Foreign.Marshal.Utils
@@ -14,14 +13,11 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "calloc" $
-    it "allocates memory with bytes of value zero" $
-      property $ do
+    it "allocates memory with bytes of value zero" $ do
         intPtr1 <- calloc :: IO (Ptr Int)
         intPtr2 <- new 0
         val1 <- peek intPtr1
         val2 <- peek intPtr2
-        res <- val1 `shouldBe` val2
+        val1 `shouldBe` val2
         free intPtr1
         free intPtr2
-        return res
-        

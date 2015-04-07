8a37052f6390bb09fc656746f1c53eb0047c5794
RyanGlScott <ryan.gl.scott@ku.edu>
2015-04-06 19:06:37 -0500

    Backport Alt to Data.Monoid.Compat

--- src/Data/Monoid/Compat.hs
+++ src/Data/Monoid/Compat.hs
@@ -15,14 +15,24 @@ module Data.Monoid.Compat (
         -- * Maybe wrappers
         -- $MaybeExamples
         First(..),
-        Last(..)
+        Last(..),
+        -- * Alternative wrapper
+        Alt(..)
   ) where
 
 import Data.Monoid as Base
 
-#if !MIN_VERSION_base(4,7,0)
+-- To import orphan Generic instances for Sum and Product
 import GHC.Generics.Compat ()
-import Prelude.Compat (Num)
+
+#if !MIN_VERSION_base(4,8,0)
+import Control.Applicative (Alternative)
+import Control.Monad (MonadPlus)
+import Prelude.Compat
+
+# if __GLASGOW_HASKELL__ >= 702
+import GHC.Generics
+# endif
 #endif
 
 #if !MIN_VERSION_base(4,5,0)
@@ -39,4 +49,24 @@ infixr 6 <>
 #if !MIN_VERSION_base(4,7,0)
 deriving instance Num a => Num (Sum a)
 deriving instance Num a => Num (Product a)
+#endif
+
+#if !MIN_VERSION_base(4,8,0)
+-- | Monoid under '<|>'.
+--
+-- /Since: 4.8.0.0/
+newtype Alt f a = Alt {getAlt :: f a}
+  deriving ( Read, Show, Eq, Ord, Num, Enum
+           , Monad, MonadPlus, Applicative, Alternative, Functor
+# if __GLASGOW_HASKELL__ >= 702
+           , Generic
+# endif
+# if __GLASGOW_HASKELL__ >= 706
+           , Generic1
+# endif
+           )
+
+instance Alternative f => Monoid (Alt f a) where
+    mempty = Alt empty
+    mappend (Alt x) (Alt y) = Alt (x <|> y)
 #endif

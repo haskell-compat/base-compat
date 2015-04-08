ed0296a76da8a01e774e3a32e4132f2595d0b19b
Simon Hengel <sol@typeful.net>
2015-04-08 15:12:55 +0800

    Minor cleanup

--- run-check.sh
+++ run-check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-runhaskell dumptypes.hs Prelude | typediff check/Prelude.types -
-runhaskell dumptypes.hs Data.Foldable | typediff check/Data.Foldable.types -
-runhaskell dumptypes.hs Data.List | typediff check/Data.List.types -
+runhaskell dumptypes.hs Prelude.Compat | typediff check/Prelude.Compat.types -
+runhaskell dumptypes.hs Data.Foldable.Compat | typediff check/Data.Foldable.Compat.types -
+runhaskell dumptypes.hs Data.List.Compat | typediff check/Data.List.Compat.types -

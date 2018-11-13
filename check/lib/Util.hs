42aa6e9f555d208db925393caed6b27eaf4a2e8c
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-11-13 14:22:31 +0200

    Use haskell-ci
    - Note: you need to use `haskell-ci` compiled against `Cabal-2.4` or
      later.
    
    - Make check/ runnable with new-test:
    
      ```
      $ export HC=ghc-7.0.4
      $ cabal new-test -w $HC type-check
      ```
    
      This patch moves files a little.
      Also ghci/new-repl wrapper prints debug INPUT/OUTPUT/NORMALISED which may be
      cleaned up.
    
      Note: `cabal new-repl` ouputs some additional lines, so
      `dumpindex` and `dumptypes` outputs aren't clean.
      Good enough, I hope.
    
    - There's a use of travis_wait:
    
      Addition of --ghc-option=-ignore-dot-ghci causes recompilation of the
      world. Unfortunately. This because such "doesn't affect compilation"
      -flags are filtered only for GHC-8.0:
    
      https://github.com/haskell/cabal/blob/b57fa37a6d84ab03512af9ab5f260bb090e58568/Cabal/Distribution/Simple/Program/GHC.hs#L55-LL58
    
      Proper fix is to extend the knowledge of cabal;
      or just let travis do (occasionally) double work.
    
    - Disable tests on 7.4.1, 7.2,1 and 7.0.1
    - However: 7.2.1 and 7.0.1 job fails, because when `cabal new-build`
      tries to install dependencies of batteries, it wants to build newer
      Cabal + dependencies for `build-type: Custom` packages.
      (it needs newer `Cabal` for some interface issues).
      This turns out to be a bad idea, as things fail due Safe Haskell bugs.
      In short: custom setup of `distributive` and `comonad` make things
      difficult.
    
      I'm not sure it's worth trying to fix. I.e. `base-compat` isn't
      installable with `cabal new-build`.

---
+++ check/lib/Util.hs
@@ -0,0 +1,143 @@
+module Util (inCabalNewRepl, normalizeLines, normalizeSignatures) where
+
+import Data.Char (isAlphaNum, isDigit, isUpper)
+import System.Environment (getEnvironment)
+import System.Process
+import qualified Text.Parsec as P
+import qualified Text.Parsec.String as P
+
+inCabalNewRepl :: String -> String -> IO String
+inCabalNewRepl module_ input_ = do
+    -- if HC variable is set, we use it to invoke 'cabal new-repl'
+    envvars <- getEnvironment
+    let mhc = lookup "HC" envvars
+    let withCompiler = maybe [] (\hc -> ["-w", hc]) mhc
+
+    putStrLn "=== INPUT ==="
+    putStrLn input
+    output <- readProcess "cabal" (args withCompiler) input
+    putStrLn "=== OUTPUT ==="
+    putStrLn output
+
+    return output
+  where
+    args withCompiler =
+        [ "new-repl"
+        , "for-repl"
+        , "--ghc-option=-ignore-dot-ghci"
+        , "--enable-tests"
+        , "--enable-benchmarks"
+        ] ++ withCompiler ++
+        [ "--"
+        ]
+
+    input = unlines
+        -- load module
+        [ ":set prompt \"\""
+        , ":l " ++ module_
+        , show randomString1
+        ]
+        ++ input_ ++
+        unlines
+            [ "", show randomString2
+            ]
+
+-- These "random" string are markers, which we output around "payload" REPL.
+-- As these "random" strings are unlikely to appear in the actual output
+-- we can be sure we extract the relevant part of the output.
+--
+randomString1 :: String
+randomString1 = "XHyIGBiBHqPo7PdVNIJV2yAwh60czbwFVKM8FhCL"
+
+randomString2 :: String
+randomString2 = "bo1vzYNeOweLxLi2zlLjbHKjXT8nClvdKKSQaehs"
+
+normalizeLines :: String -> String
+normalizeLines
+    = unlines
+    . joinIndentedLines Nothing
+    . lines
+  where
+    joinIndentedLines :: Maybe String ->  [String] -> [String]
+    joinIndentedLines Nothing  []  = []
+    joinIndentedLines (Just l) [] = [l]
+    joinIndentedLines Nothing  (l : ls) = joinIndentedLines (Just l) ls
+    joinIndentedLines (Just p) ((' ':' ':l) : ls) =
+        joinIndentedLines (Just $ p ++ ' ' : l) ls
+    joinIndentedLines (Just p) (l : ls) =
+        p : joinIndentedLines (Just l) ls
+
+normalizeSignatures :: String -> String
+normalizeSignatures
+    = unlines
+    . map stripModuleIdentifiers   -- strip package-prefixes from types
+    . takeWhile (/= show randomString2)
+    . drop 1
+    . dropWhile (/= show randomString1)
+    . joinIndentedLines Nothing
+    . lines
+  where
+    --
+    -- @
+    -- foo
+    --    :: a
+    --    -> b
+    -- @
+    --
+    -- into
+    --
+    -- @
+    -- foo :: a -> b
+    -- @
+    joinIndentedLines :: Maybe String ->  [String] -> [String]
+    joinIndentedLines Nothing  []  = []
+    joinIndentedLines (Just l) [] = [l]
+    joinIndentedLines Nothing  (l : ls) = joinIndentedLines (Just l) ls
+    joinIndentedLines (Just p) ((' ':' ':l) : ls) =
+        joinIndentedLines (Just $ p ++ ' ' : l) ls
+    joinIndentedLines (Just p) (l : ls) =
+        p : joinIndentedLines (Just l) ls
+
+    -- regex is better than manual parsing
+    --
+    -- replace
+    --
+    -- @semigroup-version@tag:C@ with @C"
+    --
+    -- so `typediff` can parse the types
+    --
+    stripModuleIdentifiers = replace parser
+      where
+        parser = do
+            -- packages: we hardcode the list of packages,
+            -- as it's simpler. Adding more (contravariant, bifunctors...)
+            -- is easy when needed; and we can be sure that we don't match
+            -- something that we shouldn't.
+            --
+            _ <- P.choice
+                [ P.try (P.string "ghc-prim")
+                , P.try (P.string "semigroups")
+                ]
+
+            -- version suffix
+            P.optional (P.char '-' >> P.many1 (P.satisfy $ \c -> isDigit c || c == '.'))
+
+            -- e.g. semigroups-0.18.5@73kDpWI4r7OHmGD2br1vF8
+
+            P.optional (P.char '@' >> P.many1 (P.satisfy isAlphaNum))
+
+            -- separator
+            _ <- P.char ':'
+
+            -- Upper-case letter: Type-constructor
+            c <- P.satisfy isUpper
+            cs <- P.getInput
+
+            return (c, cs)
+
+replace :: P.Parser (Char, String) -> String -> String
+replace p = go where
+    go [] = []
+    go str@(c : cs) = case P.runParser p () "<input>" str of
+        Left _err       -> c  : go cs
+        Right (c', cs') -> c' : go cs'

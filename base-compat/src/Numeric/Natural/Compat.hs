b9902a680c16837cfc9b806b1aa51214ace72391
Ryan Scott <ryan.gl.scott@gmail.com>
2023-01-22 16:35:05 -0500

    Backport minusNaturalMaybe to Numeric.Natural.Compat
    Addresses one check box in #24.

--- base-compat/src/Numeric/Natural/Compat.hs
+++ base-compat/src/Numeric/Natural/Compat.hs
@@ -1,10 +1,18 @@
 {-# LANGUAGE CPP, NoImplicitPrelude #-}
+#if __GLASGOW_HASKELL__ >= 702
+{-# LANGUAGE Trustworthy #-}
+#endif
 module Numeric.Natural.Compat (
 #if MIN_VERSION_base(4,8,0)
   module Base
+, minusNaturalMaybe
 #endif
 ) where
 
 #if MIN_VERSION_base(4,8,0)
 import Numeric.Natural as Base
+
+# if !(MIN_VERSION_base(4,18,0))
+import GHC.Natural (minusNaturalMaybe)
+# endif
 #endif
